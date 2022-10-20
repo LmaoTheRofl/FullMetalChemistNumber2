53f8a7200158ebbfc9a01297a57b141d74f76838
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-10-20 20:20:56 +0300

    Start

--- src/ParseHer.java
+++ src/ParseHer.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ParseHer {
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ParseHer {
+
+    final static private String[]
+            RADICALS = {"meth", "eth", "prop", "but", "pent", "hex", "hept", "oct", "non", "dec", "undec", "dodec", "tridec", "tetradec", "pentadec", "hexadec", "heptadec", "octadec", "nonadec"},
+            MULTIPLIERS = {"di", "tri",  "tetra", "penta", "hexa", "hepta", "octa", "nona", "deca", "undeca", "dodeca", "trideca", "tetradeca", "pentadeca", "hexadeca", "heptadeca", "octadeca", "nonadeca"},
+            SUFFIXES = {"ol", "al", "one", "oic acid", "carboxylic acid", "oate", "ether", "amide", "amine", "imine", "benzene", "thiol", "phosphine", "arsine"},
+            PREFIXES = {"cyclo", "hydroxy", "oxo", "carboxy", "oxycarbonyl", "anoyloxy", "formyl", "oxy", "amido", "amino", "imino", "phenyl", "mercapto", "phosphino", "arsino", "fluoro", "chloro", "bromo", "iodo"},
+            ALKANES = {"ane"},
+            ALKENES = {"ene"},
+            ALKYNES = {"yne"},
+            ALKYLES = {"yl"};
+
+
+    public ParseHer(String name) {
+        // Do whatever you want here...
+    }
+
+    public static Map<String,Integer> parse(String name) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] arr = name.split("");
+
+        // Parse the name given as argument in the constructor and output the Map representing the raw formula
+        return null;
+    }
 }
